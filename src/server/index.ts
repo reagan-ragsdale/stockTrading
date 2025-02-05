d880658dcac72f6800c3c9348182a2a7d8baff1c
Reagan Ragsdale <rayrags10@gmail.com>
2025-02-05 13:38:24 -0600

    asdf

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,6 +4,12 @@ import session from "cookie-session"
 import cors from 'cors'
 
 const app = express()
+app.use(cors({
+  origin: 'https://stocktrading.up.railway.app', // use your actual domain name (or localhost), using * is not recommended
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+  credentials: true
+}))
 app.use(
     session({
       secret: process.env["SESSION_SECRET"] || "my secret"
@@ -13,12 +19,7 @@ app.use(api)
 
 const frontendFiles = process.cwd() + "/dist/stock-trading/browser";
 app.use(express.static(frontendFiles));
-app.use(cors({
-  origin: 'https://stocktrading.up.railway.app', // use your actual domain name (or localhost), using * is not recommended
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
-  credentials: true
-}))
+
 app.get("/*", (_, res) => {
   res.sendFile(frontendFiles + "/index.html");
 });
