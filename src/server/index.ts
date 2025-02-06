b4dfbc5597378ed4d1bf62bb3d9b6e0cc83de683
Reagan Ragsdale <rayrags10@gmail.com>
2025-02-06 09:48:26 -0600

    asd

--- src/server/index.ts
+++ src/server/index.ts
@@ -27,7 +27,7 @@ app.use(cors({
   origin: "https://stocktrading.up.railway.app"
 }))
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://stocktrading.up.railway.app");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
