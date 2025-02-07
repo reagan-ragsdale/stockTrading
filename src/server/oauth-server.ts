54750066b06332d112ffffd78b91d25aab58c396
Reagan Ragsdale <rayrags10@gmail.com>
2025-02-07 09:53:51 -0600

    asd

--- src/server/oauth-server.ts
+++ src/server/oauth-server.ts
@@ -6,7 +6,7 @@ export const oauthCall = async (code: string): Promise<string[]> => {
     console.log(userKeys)
     let credentials = Buffer.from(`${userKeys.appKey}:${userKeys.appSecret}`).toString('base64')
     let payload = new URLSearchParams({
-        grant_type: 'authorization_code', code: code, redirect_uri: 'https://stocktrading.up.railway.app'
+        grant_type: 'authorization_code', code: code, redirect_uri: 'https://stocktrading.up.railway.app/auth'
     })
     const url = 'https://api.schwabapi.com/v1/oauth/token';
     const options = {
