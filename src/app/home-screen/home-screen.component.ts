2a621c16c99665f14ba048c3df73ed1cb7203b3b
Reagan Ragsdale <rayrags10@gmail.com>
2025-02-11 14:07:55 -0600

    asd

--- src/app/home-screen/home-screen.component.ts
+++ src/app/home-screen/home-screen.component.ts
@@ -389,6 +389,7 @@ export class HomeScreenComponent implements OnInit, OnDestroy {
 
   isLoading: boolean = true;
   async ngOnInit() {
+    await SimFinance.insertOrUpdateAmount(100)
     //this.isLoading = true;
     Chart.register(annotationPlugin);
     Chart.register(...registerables)
