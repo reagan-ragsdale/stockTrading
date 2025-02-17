bfede9ef1110d7a02bd958f63581b32636baca3a
Reagan Ragsdale <rayrags10@gmail.com>
2025-02-17 10:39:12 -0600

    sd

--- src/app/apiCalls/loadNewAccessToken.ts
+++ src/app/apiCalls/loadNewAccessToken.ts
@@ -2,9 +2,12 @@ import { AuthController } from "../../shared/controllers/AuthController.js"
 import { OAuthContoller } from "../../shared/controllers/OAuthController.js"
 
 export const loadNewToken = async () => {
+    console.log('here in token')
     let token = await OAuthContoller.sendRefreshCall()
     if(token != ''){
         await AuthController.updateAccessToken(token)
+        console.log('updated token')
     }
+    
     
 }
