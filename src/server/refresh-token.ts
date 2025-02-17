5df6aa007291bb3f6d19ba2ecead59c0b7f8500b
Reagan Ragsdale <rayrags10@gmail.com>
2025-02-17 12:26:48 -0600

    asd

--- src/server/refresh-token.ts
+++ src/server/refresh-token.ts
@@ -1,26 +1,28 @@
-import { Buffer} from 'node:buffer'
+import { Buffer } from 'node:buffer'
 import { AuthController } from '../shared/controllers/AuthController.js'
 import { URLSearchParams } from 'node:url';
 export const refreshCall = async (): Promise<string> => {
     console.log('here in before fetch')
-    let userKeys = await AuthController.getKeyPairs()
-    let credentials = Buffer.from(`${userKeys.appKey}:${userKeys.appSecret}`).toString('base64')
-    let payload = new URLSearchParams({
-        grant_type: 'refresh_token', refresh_token: userKeys.refreshToken
-    })
-    const url = 'https://api.schwabapi.com/v1/oauth/token';
-    const options = {
-        method: 'POST',
-        headers: {
-            'Authorization': `Basic ${credentials}`,
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: payload
-        
-    };
-
-
     try {
+        let userKeys = await AuthController.getKeyPairs()
+        console.log(userKeys)
+        let credentials = Buffer.from(`${userKeys.appKey}:${userKeys.appSecret}`).toString('base64')
+        let payload = new URLSearchParams({
+            grant_type: 'refresh_token', refresh_token: userKeys.refreshToken
+        })
+        const url = 'https://api.schwabapi.com/v1/oauth/token';
+        const options = {
+            method: 'POST',
+            headers: {
+                'Authorization': `Basic ${credentials}`,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: payload
+
+        };
+
+
+
         const response = await fetch(url, options);
         const result = await response.json();
         console.log(result)
