906d3cb175203d2d339e1cdbc5ff0ce0b74711c7
Reagan Ragsdale <rayrags10@gmail.com>
2025-02-17 09:54:32 -0600

    as

--- src/app/test-screen/test-screen.component.ts
+++ src/app/test-screen/test-screen.component.ts
@@ -617,7 +617,6 @@ export class TestScreenComponent implements OnInit, OnDestroy {
     await this.getStockData()
     this.createOrUpdateChart()
     this.createVolumeChart()
-    await RegFinanceController.insertOrUpdateAmountReg(250)
     
 
     /*  this.unsubscribe = rhRepo
