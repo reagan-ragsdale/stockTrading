d7d4e3114ac56ddd5fe1a0e9f6147ec8f8677454
Reagan Ragsdale <rayrags10@gmail.com>
2025-02-18 14:56:50 -0600

    sd

--- src/server/insertStockData.ts
+++ src/server/insertStockData.ts
@@ -2,45 +2,45 @@ import { AuthController } from "../shared/controllers/AuthController.js"
 import { StockHistoryController } from "../shared/controllers/StockHistoryController.js"
 import { dbCurrentDayStockDataRepo } from "../shared/tasks/dbCurrentDayStockData.js"
 import { dbTokenRepo, DbTOkens } from "../shared/tasks/dbTokens.js"
-import WebSocket from 'ws';
+import { WebSocket } from 'ws';
 
 export const insertCall = async (): Promise<void> => {
 
 
-    const userData = await dbTokenRepo.findFirst({id: {'!=': ''}}) as DbTOkens
+    const userData = await dbTokenRepo.findFirst({ id: { '!=': '' } }) as DbTOkens
 
     const schwabWebsocket = new WebSocket(userData.streamerSocketUrl)
     let hasBeenSent = false
     const loginMsg = {
         "requests": [
-          {
-            "service": "ADMIN",
-            "requestid": "0",
-            "command": "LOGIN",
-            "SchwabClientCustomerId": userData.schwabClientCustomerId,
-            "SchwabClientCorrelId": userData.schwabClientCorrelId,
-            "parameters": {
-              "Authorization": userData.accessToken,
-              "SchwabClientChannel": userData.schwabClientChannel,
-              "SchwabClientFunctionId": userData.schwabClientFunctionId
+            {
+                "service": "ADMIN",
+                "requestid": "0",
+                "command": "LOGIN",
+                "SchwabClientCustomerId": userData.schwabClientCustomerId,
+                "SchwabClientCorrelId": userData.schwabClientCorrelId,
+                "parameters": {
+                    "Authorization": userData.accessToken,
+                    "SchwabClientChannel": userData.schwabClientChannel,
+                    "SchwabClientFunctionId": userData.schwabClientFunctionId
+                }
             }
-          }
         ]
-      }
-      const aaplDataMsg = {
+    }
+    const aaplDataMsg = {
         //add the level 2 data to this
         "requests": [
-          {
-            "service": "LEVELONE_EQUITIES",
-            "requestid": "1",
-            "command": "SUBS",
-            "SchwabClientCustomerId": userData.schwabClientCustomerId,
-            "SchwabClientCorrelId": userData.schwabClientCorrelId,
-            "parameters": {
-              "keys": "AAPL",
-              "fields": "0,1,2,3,4,5,6,7,8,9,10,33"
-            }
-          }/* ,
+            {
+                "service": "LEVELONE_EQUITIES",
+                "requestid": "1",
+                "command": "SUBS",
+                "SchwabClientCustomerId": userData.schwabClientCustomerId,
+                "SchwabClientCorrelId": userData.schwabClientCorrelId,
+                "parameters": {
+                    "keys": "AAPL",
+                    "fields": "0,1,2,3,4,5,6,7,8,9,10,33"
+                }
+            }/* ,
           {
             "service": "NYSE_BOOK",
             "requestid": "23",
@@ -53,37 +53,32 @@ export const insertCall = async (): Promise<void> => {
             }
           } */
         ]
-      }
-
-      schwabWebsocket.on('open', function open() {
+    }
+    schwabWebsocket.on('open', () => {
         schwabWebsocket.send(JSON.stringify(loginMsg))
-      })
+    })
+    schwabWebsocket.on('message', async (event) => {
+        let newEvent = JSON.parse(event.toString())
+        let data = newEvent.data
+        console.log(data)
 
-      schwabWebsocket.on('message', async function message(event: any) {
-  
-        console.log(event)
-        let data = JSON.parse(event.data)
-       
-  
-        if (Object.hasOwn(data, 'response')) {
-          if (data.response[0].content.code == 0 && hasBeenSent == false) {
-            schwabWebsocket!.send(JSON.stringify(aaplDataMsg))
-            console.log('send aapl')
-            hasBeenSent = true
-          }
+        /* if (Object.hasOwn(data, 'response')) {
+            if (data.response[0].content.code == 0 && hasBeenSent == false) {
+                schwabWebsocket.send(JSON.stringify(aaplDataMsg))
+                console.log('send aapl')
+                hasBeenSent = true
+            }
         }
         if (Object.hasOwn(data, 'data') && hasBeenSent == true) {
-          if (Object.hasOwn(data.data[0].content[0], '3')) {
-            await dbCurrentDayStockDataRepo.insert({stockName: data.data[0].content[0].key, stockPrice: data.data[0].content[0]['3'], time: Number(data.data[0].timestamp)})
-          }
-  
-        }
-        /* 
-        if (count == 0) {
-          this.schwabWebsocket.send(JSON.stringify(aaplDataMsg))
-        }
-        count++ */
-      })
+            if (Object.hasOwn(data.data[0].content[0], '3')) {
+                await dbCurrentDayStockDataRepo.insert({ stockName: data.data[0].content[0].key, stockPrice: data.data[0].content[0]['3'], time: Number(data.data[0].timestamp) })
+            }
+
+        } */
+    });
+
+      
+      
 
 }
 
