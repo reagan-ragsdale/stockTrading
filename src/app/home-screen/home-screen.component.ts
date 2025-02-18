8360da67c7b55bf6e2f72d64a3cb28fc92a5cee9
Reagan Ragsdale <rayrags10@gmail.com>
2025-02-18 09:25:52 -0600

    asd

--- src/app/home-screen/home-screen.component.ts
+++ src/app/home-screen/home-screen.component.ts
@@ -536,7 +536,7 @@ export class HomeScreenComponent implements OnInit, OnDestroy {
       orderType: buyOrSell,
       stockName: this.selectedStockName,
       stockPrice: this.selectedStockCurrent,
-      shareQty: this.userSimFinData[0].spending / this.selectedStockCurrent,
+      shareQty: buyOrSell == 'Buy' ? this.userSimFinData[0].spending / this.selectedStockCurrent : this.selectedStockData.shareQty,
       orderTime: this.chartData.time[this.chartData.time.length - 1]
     }
     await OrderService.executeOrder(order, this.selectedStockHistoryData[0])
