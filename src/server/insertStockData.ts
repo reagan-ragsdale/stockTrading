ec336468cc229c78fc418e842a4ab48cf6868d15
Reagan Ragsdale <rayrags10@gmail.com>
2025-02-21 12:55:25 -0600

    asd

--- src/server/insertStockData.ts
+++ src/server/insertStockData.ts
@@ -59,6 +59,7 @@ export const insertCall = async (): Promise<void> => {
     })
     schwabWebsocket.on('message', async (event) => {
         let newEvent = JSON.parse(event.toString())
+        console.log(newEvent)
 
         //console.log(newEvent.response[0].content)
 
