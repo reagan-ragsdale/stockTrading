206040479802ef2cc7e82da3eda8699624e87b62
Reagan Ragsdale <rayrags10@gmail.com>
2025-02-27 08:46:26 -0600

    ads

--- src/server/insertStockData.ts
+++ src/server/insertStockData.ts
@@ -37,7 +37,7 @@ export const insertCall = async (): Promise<void> => {
                 "SchwabClientCustomerId": userData.schwabClientCustomerId,
                 "SchwabClientCorrelId": userData.schwabClientCorrelId,
                 "parameters": {
-                    "keys": "AAPL, MSFT",
+                    "keys": "AAPL, MSFT, PLTR",
                     "fields": "0,1,2,3,4,5,6,7,8,9,10,33"
                 }
             }/* ,
