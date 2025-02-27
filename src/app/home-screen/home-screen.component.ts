25ec26e1d43d55bdce26c3ed53e680f804122784
Reagan Ragsdale <rayrags10@gmail.com>
2025-02-27 10:30:44 -0600

    asd

--- src/app/home-screen/home-screen.component.ts
+++ src/app/home-screen/home-screen.component.ts
@@ -179,10 +179,10 @@ export class HomeScreenComponent implements OnInit, OnDestroy {
       else {
         let returnVal = AnalysisService.trendTrading(this.chartData.history.slice((this.chartData.history.length - this.trendAlgoStartingPoint) * -1), this.chartData.time.slice((this.chartData.time.length - this.trendAlgoStartingPoint) * -1),this.selectedStockHistoryData)
         console.log(returnVal)
-        this.stockChart.options.plugins.annotation.annotations.trendLine.xMin = returnVal.xMin
-        this.stockChart.options.plugins.annotation.annotations.trendLine.xMax = returnVal.xMax
-        this.stockChart.options.plugins.annotation.annotations.trendLine.yMin = returnVal.yMin
-        this.stockChart.options.plugins.annotation.annotations.trendLine.yMax = returnVal.yMax
+        this.stockChart.options.plugins.annotation.annotations.trendLine.xMin = returnVal.yMin
+        this.stockChart.options.plugins.annotation.annotations.trendLine.xMax = returnVal.yMax
+        this.stockChart.options.plugins.annotation.annotations.trendLine.yMin = returnVal.xMin
+        this.stockChart.options.plugins.annotation.annotations.trendLine.yMax = returnVal.xMax
       }
 
       console.log(shouldPlaceOrder)
