83e9ecfcd5e6d468a539f485538cf215fa84b79b
Reagan Ragsdale <rayrags10@gmail.com>
2025-02-28 14:16:32 -0600

    nki

--- src/server/spawnSocketWorker.ts
+++ src/server/spawnSocketWorker.ts
@@ -2,5 +2,5 @@ import { Worker } from 'node:worker_threads'
 
 export const startWorker = async (): Promise<void> => {
     console.log('start worker')
-    const worker = new Worker('./insertStockData.ts')
+    const worker = new Worker('./insertStockData')
 }
