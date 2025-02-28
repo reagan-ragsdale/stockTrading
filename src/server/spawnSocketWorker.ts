d590643fd3cfb8eaa92b2de3c3fea67f487556ee
Reagan Ragsdale <rayrags10@gmail.com>
2025-02-28 14:39:47 -0600

    asd

--- src/server/spawnSocketWorker.ts
+++
@@ -1,6 +0,0 @@
-import { Worker } from 'node:worker_threads'
-
-export const startWorker = async (): Promise<void> => {
-    console.log('start worker')
-    const worker = new Worker('./server/insertStockData.ts')
-}
