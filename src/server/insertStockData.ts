c10b55bd884b83cc07947693a8f7486267686fac
Reagan Ragsdale <rayrags10@gmail.com>
2025-03-05 12:22:05 -0600

    dsf

--- src/server/insertStockData.ts
+++ src/server/insertStockData.ts
@@ -39,7 +39,7 @@ export const socketCall = async (): Promise<void> => {
                 "SchwabClientCustomerId": userData.schwabClientCustomerId,
                 "SchwabClientCorrelId": userData.schwabClientCorrelId,
                 "parameters": {
-                    "keys": "AAPL, MSFT, PLTR, F",
+                    "keys": "AAPL, MSFT, PLTR",
                     "fields": "0,1,2,3,4,5,6,7,8,9,10,33"
                 }
             }
--- src/server/oauth-server.ts
+++ src/server/oauth-server.ts
@@ -2,7 +2,9 @@ import { Buffer} from 'node:buffer'
 import { AuthController } from '../shared/controllers/AuthController.js'
 import { URLSearchParams } from 'node:url';
 export const oauthCall = async (code: string): Promise<string[]> => {
+    console.log('here 1')
     let userKeys = await AuthController.getKeyPairs()
+    console.log(userKeys)
     let credentials = Buffer.from(`${userKeys.appKey}:${userKeys.appSecret}`).toString('base64')
     let payload = new URLSearchParams({
         grant_type: 'authorization_code', code: code, redirect_uri: 'https://stocktrading.up.railway.app/auth'
@@ -22,6 +24,7 @@ export const oauthCall = async (code: string): Promise<string[]> => {
     try {
         const response = await fetch(url, options);
         const result = await response.json();
+        console.log(result)
         let refreshToken = result['refresh_token']
         let accessToken = result['access_token']
         return [accessToken, refreshToken]
