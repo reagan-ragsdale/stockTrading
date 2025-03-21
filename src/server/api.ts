aed1cca8f17061fffedb8a7abacaf538b0068dd3
Reagan Ragsdale <rayrags10@gmail.com>
2025-03-21 08:32:32 -0500

    ad

--- src/server/api.ts
+++ src/server/api.ts
@@ -80,8 +80,8 @@ export const api = remultExpress({
     })  */ ,
     initRequest
     ,initApi: async () => {
-      //socketCall(),
-      cron.schedule('0 2 * * 1-5', () => socketCall())
+      socketCall(),
+      cron.schedule('0 12 * * 1-5', () => socketCall())
       //cron.schedule('30 14 * * *', () => insertCall())
       cron.schedule('0 4 * * 1', () => resetTokens()),
       cron.schedule('*/25 * * * *', () => loadNewToken()),
