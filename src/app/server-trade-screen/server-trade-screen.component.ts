0f9cdb87f219eaee9224422198c30bd0150849c5
Reagan Ragsdale <rayrags10@gmail.com>
2025-04-16 16:49:53 -0500

    aas

--- src/app/server-trade-screen/server-trade-screen.component.ts
+++ src/app/server-trade-screen/server-trade-screen.component.ts
@@ -1150,30 +1150,30 @@ export class ServerTradeScreenComponent implements OnInit {
     this.listOfLast40Days = []
     this.listOfLast5Days = []
     
-    for(let i = this.selectedInterDayStockData.length - 1; i >= 0; i--){
+    for(let i = this.selectedInterDayStockData.length - 1; i >= this.interDayLongSma; i--){
       let longSma: number = 0
       for(let j = 0; j < this.interDayLongSma; j++){
         longSma += this.selectedInterDayStockData[i - j].close
       }
       this.listOfLast200Days.unshift({stockName: this.selectedStockName, close: this.selectedInterDayStockData[i].close, avg: longSma / this.interDayLongSma, date: new Date(this.selectedInterDayStockData[i].date).toLocaleDateString()})
     }
-    for(let i = this.selectedInterDayStockData.length - 1; i >= 0; i--){
+    for(let i = this.selectedInterDayStockData.length - 1; i >= this.interDayLongSma; i--){
       let mediumSma: number = 0
       for(let j = 0; j < this.interDayMediumSma; j++){
         mediumSma += this.selectedInterDayStockData[i - j].close
       }
       this.listOfLast40Days.unshift({stockName: this.selectedStockName, close: this.selectedInterDayStockData[i].close, avg: mediumSma / this.interDayMediumSma, date: new Date(this.selectedInterDayStockData[i].date).toLocaleDateString()})
     }
-    for(let i = this.selectedInterDayStockData.length - 1; i >= 0; i--){
+    for(let i = this.selectedInterDayStockData.length - 1; i >= this.interDayLongSma; i--){
       let shortSma: number = 0
       for(let j = 0; j < this.interDayShortSma; j++){
         shortSma += this.selectedInterDayStockData[i - j].close
       }
       this.listOfLast5Days.unshift({stockName: this.selectedStockName, close: this.selectedInterDayStockData[i].close, avg: shortSma / this.interDayShortSma, date: new Date(this.selectedInterDayStockData[i].date).toLocaleDateString()})
     }
-    let lengthOfLongArray = this.listOfLast200Days.length
-    this.listOfLast40Days = this.listOfLast40Days.slice(lengthOfLongArray * -1)
-    this.listOfLast5Days = this.listOfLast5Days.slice(lengthOfLongArray * -1)
+    //let lengthOfLongArray = this.listOfLast200Days.length
+    //this.listOfLast40Days = this.listOfLast40Days.slice(lengthOfLongArray * -1)
+    //this.listOfLast5Days = this.listOfLast5Days.slice(lengthOfLongArray * -1)
   }
   updateChart() {
     this.stockChart.data.datasets[0].data = this.listOfLast200Days.map(e => e.close)
