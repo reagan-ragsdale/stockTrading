0295b858cd027bb6d1a44746ef00b791d5743555
Reagan Ragsdale <rayrags10@gmail.com>
2025-04-27 17:49:14 -0500

    ads

--- src/app/services/reusedFunctions.ts
+++ src/app/services/reusedFunctions.ts
@@ -1,9 +1,8 @@
 export class reusedFunctions {
     static epochToLocalTime(epoch: number): string {
-        const date = new Date(epoch); // Convert epoch seconds to milliseconds
-        const hours = String(date.getHours()).padStart(2, '0');
-        const minutes = String(date.getMinutes()).padStart(2, '0');
-        return `${hours}:${minutes}`;
+        return new Date(epoch).toLocaleTimeString('en-US', {
+            timeZone: 'America/Chicago',
+        })
     }
     static formatDate(date: Date): string {
         const day = String(date.getDate()).padStart(2, '0'); // Ensure two digits
