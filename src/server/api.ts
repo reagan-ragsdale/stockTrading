6a806070971733936b8a89faeb49030389115e60
Reagan Ragsdale <rayrags10@gmail.com>
2025-06-13 11:12:22 -0500

    asd

--- src/server/api.ts
+++ src/server/api.ts
@@ -103,7 +103,7 @@ export const api = remultExpress({
     //orig 30 13
     if (process.env['environment'] == 'Prod') {
       cron.schedule('0 11 * * 2-6', () => getDailyStockInfo()),
-        cron.schedule('15 16 * * 1-5', () => socketCall()),
+        cron.schedule('20 16 * * 1-5', () => socketCall()),
 
         cron.schedule('0 9 * * 1,4', () => resetTokens()),
         cron.schedule('*/25 * * * *', () => loadNewToken()),
