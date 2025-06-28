d9d521bd0c050de35d23157fc6a877ae1a4c4066
Reagan Ragsdale <rayrags10@gmail.com>
2025-06-27 22:34:49 -0500

    bnb

--- src/server/api.ts
+++ src/server/api.ts
@@ -111,8 +111,7 @@ export const api = remultExpress({
         cron.schedule('0 9 * * 1,4', () => resetTokens()),
         cron.schedule('*/25 * * * *', () => loadNewToken()),
         cron.schedule('45 20 * * 1-5 ', () => loadDailyDataIntoHistory()),
-        cron.schedule('10 20 * * 1-5 ', () => LoggerController.sendEmailCall()),
-        getDailyStockInfo()
+        cron.schedule('10 20 * * 1-5 ', () => LoggerController.sendEmailCall())
     }
 
     //LoggerController.sendEmailCall()
