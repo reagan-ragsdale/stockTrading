12b6e69f3e445e2e76e95c42d21b154b98281f9f
Reagan Ragsdale <rayrags10@gmail.com>
2025-06-27 22:09:31 -0500

    ad

--- src/server/getHistoryStockData.ts
+++ src/server/getHistoryStockData.ts
@@ -1,21 +1,22 @@
 import { dbTokenRepo, DbTOkens } from '../shared/tasks/dbTokens.js';
 export const getHistoryStockData = async (stockName: string, time: number): Promise<any> => {
-    const userData = await dbTokenRepo.findFirst({ id: 'asdfghjkl' }) as DbTOkens
-    const url = `https://api.schwabapi.com/marketdata/v1/pricehistory?symbol=${stockName}&periodType=month&period=1&frequencyType=daily&frequency=1&startDate=${time}&needExtendedHoursData=false&needPreviousClose=false`;
-    const options = {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${userData.accessToken}`
-      }
-    };
-    try {
-      const response = await fetch(url, options);
-      const result = await response.json();
-      return result
+  const userData = await dbTokenRepo.findFirst({ id: 'asdfghjkl' }) as DbTOkens
+  const url = `https://api.schwabapi.com/marketdata/v1/pricehistory?symbol=${stockName}&periodType=month&period=1&frequencyType=daily&frequency=1&startDate=${time}&needExtendedHoursData=false&needPreviousClose=false`;
+  const options = {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${userData.accessToken}`
     }
-    catch (error: any) {
-      console.log('getHistoryStockData server: ' + error.message)
-      return []
-    }
+  };
+  try {
+    const response = await fetch(url, options);
+    console.log(response)
+    const result = await response.json();
+    return result
+  }
+  catch (error: any) {
+    console.log('getHistoryStockData server: ' + error.message)
+    return []
+  }
 }
 
