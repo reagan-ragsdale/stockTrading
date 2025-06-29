9e70debe996f7729e818d9cb1e75e12b6267d077
Reagan Ragsdale <rayrags10@gmail.com>
2025-06-29 14:00:49 -0500

    asd

--- src/server/getIntraDatHistory.ts
+++ src/server/getIntraDatHistory.ts
@@ -4,10 +4,13 @@ export const getIntraDayHistoryData = async (stockName: string, date: string): P
     const url = `https://api.polygon.io/v3/trades/${stockName}?timestamp=${date}&order=asc&limit=50000&sort=timestamp&apiKey=${apiKey}`;
     try {
         const response = await fetch(url);
-        console.log(response)
         if (response.ok) {
             const result = await response.json();
-            return result
+            return result.results
+        }
+        else {
+            console.log(response)
+            return []
         }
 
     }
