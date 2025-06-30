822f30312e1574bc0f5861734414cadb9327701a
Reagan Ragsdale <rayrags10@gmail.com>
2025-06-30 16:50:38 -0500

    ads

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,23 +1,24 @@
 import { APP_INITIALIZER, ApplicationConfig, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import {MatCardModule} from '@angular/material/card';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 
 import { routes } from './app.routes';
 import { AuthGuard } from './app-auth-guard.js';
-import {  MatButtonModule } from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { AuthController } from '../shared/controllers/AuthController.js';
 import { remult } from 'remult';
 import { api } from '../server/api';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material/core'
+import { MatNativeDateModule } from '@angular/material/core'
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), 
-    { provide: provideAppInitializer(initApp()), useFactory: initApp, multi: true },
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimations(),
+  { provide: provideAppInitializer(initApp()), useFactory: initApp, multi: true },
     MatCardModule,
     MatToolbarModule,
     MatFormFieldModule,
@@ -26,16 +27,16 @@ export const appConfig: ApplicationConfig = {
     AuthGuard,
     MatButtonModule,
     MatDatepickerModule,
-    MatNativeDateModule 
+    MatNativeDateModule
   ]
-  
+
 };
 
 export function initApp() {
   const loadCurrentUserBeforeAppStarts = async () => {
     //remult.user = await AuthController.currentUser()
     let userThing = remult.initUser()
-    
+
   }
   return loadCurrentUserBeforeAppStarts
 }
